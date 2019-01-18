d609e6b40b9c514c08699fab3b017989c31ad78f
imtiazaminsajid <ambition.sajid@gmail.com>
2019-01-19 01:26:10 +0600

    Pulse View done

--- app/src/main/java/com/example/ambit/pulseview/MainActivity.java
+++ app/src/main/java/com/example/ambit/pulseview/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.ambit.pulseview;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.gigamole.library.PulseView;
 
 public class MainActivity extends AppCompatActivity {
+
+    PulseView pulseView;
+    Button startButton, stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pulseView = findViewById(R.id.pulseView);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pulseView.startPulse();
+
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pulseView.finishPulse();
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+
+        builder.setMessage("Do you want to Exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+
+                    }
+                })
+                .setNegativeButton("Cencel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                })
+                .create().show();
     }
 }
